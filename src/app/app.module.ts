e8bf2867b75ff63a349421d8d12f172d26418805
Nicolas <niko.le@free.fr>
2023-01-26 17:25:15 +0100

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoveStoriesComponent } from './love-stories/love-stories.component';
 import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoveStoriesComponent,
     TimelineComponent
   ],
   imports: [
