12ffcab3fcaeb2ec639ab68e0e679752b72298a8
Nicolas <niko.le@free.fr>
2023-02-05 01:45:33 +0100

    WIP

--- src/app/models/chapter.ts
+++ src/app/models/chapter.ts
@@ -1,3 +1,5 @@
+import { Tag } from "./tag";
+
 export class Chapter {
     title: string;
     content: string;
@@ -5,5 +7,5 @@ export class Chapter {
     endDate: Date;
     narrated: string;
     people: string[];
-    tags: string[];
+    tags: Tag[];
 }
