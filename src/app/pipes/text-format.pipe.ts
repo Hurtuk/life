15850aa4e99015a2ac2774f2f7f85fa5ed9343d9
Nicolas <niko.le@free.fr>
2023-02-10 00:06:06 +0100

    WIP

--- src/app/pipes/text-format.pipe.ts
+++ src/app/pipes/text-format.pipe.ts
@@ -7,16 +7,22 @@ export class TextFormatPipe implements PipeTransform {
 
   transform(value: string, autop = true): string {
     let result = value
-      .replace(/ ([\?!:;])/gi, "&nbsp;$1")
-      .replace(/« /gi, "«&nbsp;")
-      .replace(/ »/gi, "&nbsp;»")
-      .replace(/\.\.\./gi, "…")
-      .replace(/'/gi, "’")
+      .replace(/ ([\?!:;])/g, "&nbsp;$1")
+      .replace(/« /g, "«&nbsp;<i>")
+      .replace(/ »/g, "</i>&nbsp;»")
+      .replace(/"([^"]+)"/g, "«&nbsp;<i>$1</i>&nbsp;»")
+      .replace(/\.\.\./g, "…")
+      .replace(/'/g, "’")
+      .replace(/\- (.+) \-/gm, "–&nbsp;$1&nbsp;–")
       .replace(/(\d)((ers?)|(ères?)|(èmes?))/gi, "$1<sup>$2</sup>")
       .replace(/\r/g, "")
       .split("\n");
     if (autop) {
-      result = result.map(v => !v ? '': ('<p>' + v + '</p>'));
+      result = result.map(v => {
+        if (!v) return '';
+        if (v.match(/^.*[^….!?\]]$/g)) return '<h2>' + v + '</h2>';
+        return '<p>' + v + '</p>';
+      });
     }
     return result.join('');
   }
